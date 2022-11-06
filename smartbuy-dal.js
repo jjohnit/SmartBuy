f2349c9654d7dc079773d947072537b01cba6a57
Johnit Jasan <jjohnit@gmail.com>
2022-11-06 13:08:44 -0600

    changes in row with location and search

--- smartbuy-dal.js
+++ smartbuy-dal.js
@@ -1,96 +1,27 @@
-/* SmartBuy DB */
-var products = [
-    {
-        id : 1,
-        name: "iPhone 14",
-        color: "Black",
-        storage: "128 GB",
-    },
-    {
-        id : 2,
-        name: "Samsung SSD",
-        color: "Black",
-        storage: "1 TB",
-    },
-    {
-        id : 3,
-        name: "Eggs",
-        type: "Organic",
-        quantity: 12,
-    },
-];
+document.getElementById('notification1').value = offers[0].offer;
 
-var stores = [
-    {
-        id: 1,
-        name: "Walmart",
-        type: "store"
-    },
-    {
-        id: 2,
-        name: "Walmart",
-        type: "online"
-    },
-    {
-        id: 3,
-        name: "Target",
-        type: "store"
-    },
-    {
-        id: 4,
-        name: "Target",
-        type: "online"
-    },
-    {
-        id: 5,
-        name: "Amazon",
-        type: "online"
-    },
-    {
-        id: 6,
-        name: "Costco",
-        type: "store"
+function getNotifications() {
+    var list = document.getElementById('notifications');
+    for (let i = 0; i < offers.length; i++) {
+        var entry = document.createElement('li');
+        for (let j = 0; j < products.length; j++) {
+            if (products[j].id == offers[i].productId) {
+                var prodname = products[j].name;
+                break;
+            }
+        }
+
+        for (let j = 0; j < stores.length; j++) {
+            if (stores[j].id == offers[i].storeId) {
+                var storename = stores[j].name;
+                break;
+            }
+        }
+
+        entry.appendChild(document.createTextNode(prodname + "\n" + offers[i].offer + " at " + storename));
+        list.appendChild(entry);
+        entry = document.createElement('hr');
+        list.appendChild(entry);
+
     }
-];
-
-var productPrices = [
-    {
-        productId: 1,
-        storeId: 1,
-        price: 1299,
-        tax: 39.99,
-        discount: 149
-    },
-    {
-        productId: 1,
-        storeId: 5,
-        price: 1299,
-        tax: 39.99,
-        deliveryCharge: 10,
-        discount: 99
-    },
-    {
-        productId: 1,
-        storeId: 6,
-        price: 1299,
-        tax: 39.99,
-        discount: 59
-    },
-    {
-        productId: 2,
-        storeId: 2,
-        price: 799,
-        tax: 19.99,
-        deliveryCharge: 9.99,
-        discount: 149
-    },
-    {
-        productId: 2,
-        storeId: 3,
-        price: 799,
-        tax: 19.99,
-        discount: 99
-    }
-];
-
-document.getElementById('notification1').value=offers[0].offer;
+}
