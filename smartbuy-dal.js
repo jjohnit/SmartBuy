4f18a68d23b0fa4caa2db4720ac1ddac2c5764c6
Johnit Jasan <jjohnit@gmail.com>
2022-11-08 17:56:47 -0600

    Format changes

--- smartbuy-dal.js
+++ smartbuy-dal.js
@@ -7,67 +7,67 @@ $(document).ready(function () {
     getSubscriptions();
 })
 
-$(document).on('click','#search-button', function(){
-var search_term = document.getElementById('search-tab').value;
-var final_prods =[];
-for (let j = 0; j < products.length; j++) {
-    var prodname=products[j].name.toLowerCase().replace(/\s/g, '');
-    if (prodname.includes(search_term.toLowerCase().replace(/\s/g, ''))){
-        final_prods.push(products[j]);
-    }
+$(document).on('click', '#search-button', function () {
+    var search_term = document.getElementById('search-tab').value;
+    var final_prods = [];
+    for (let j = 0; j < products.length; j++) {
+        var prodname = products[j].name.toLowerCase().replace(/\s/g, '');
+        if (prodname.includes(search_term.toLowerCase().replace(/\s/g, ''))) {
+            final_prods.push(products[j]);
+        }
 
-}
-setPage('search-results');
-createTable_searchresults(final_prods);
+    }
+    setPage('search-results');
+    createTable_searchresults(final_prods);
 })
 
 
-function createTable_searchresults(final_prods){  
-tableElem = document.getElementById("search-results-table");
-tableElem.innerHTML="";
-for (let i = 0; i < final_prods.length; i++){    
-  rowElem = document.createElement('tr');
-  colElem = document.createElement('td');
-  colElem.innerHTML="";
-  for (var key in final_prods[i]) {
-    if (key.toString()!='id'){
-        colElem.innerHTML=colElem.innerHTML+" "+final_prods[i][key].toString();
-    }
-  }
-
-
-  var prices=[];
-  var stores_final=[];
-  for (let j = 0;j< productPrices.length;j++){
-    if( productPrices[j].productId==final_prods[i].id){
-        var total = productPrices[j].price + 
-                    productPrices[j].tax + 
-                    productPrices[j].deliveryCharge - 
-                    productPrices[j].discount;            
-        prices.push(total);
-        stores_final.push(productPrices[j].storeId);
-    }
-  }
-  
-  colElem.innerHTML=colElem.innerHTML+"<br />";
-  for(let j=0;j<stores_final.length;j++){
-    for(let k=0;k<stores.length;k++){
-        if(stores[k].id==stores_final[j]){
-            colElem.innerHTML=colElem.innerHTML+"<img src='./assets/"+stores[k].icon+"' class='icon-image'>"; 
-            break;
+function createTable_searchresults(final_prods) {
+    tableElem = document.getElementById("search-results-table");
+    tableElem.innerHTML = "";
+    for (let i = 0; i < final_prods.length; i++) {
+        rowElem = document.createElement('tr');
+        colElem = document.createElement('td');
+        colElem.innerHTML = "";
+        for (var key in final_prods[i]) {
+            if (key.toString() != 'id') {
+                colElem.innerHTML = colElem.innerHTML + " " + final_prods[i][key].toString();
+            }
         }
+
+
+        var prices = [];
+        var stores_final = [];
+        for (let j = 0; j < productPrices.length; j++) {
+            if (productPrices[j].productId == final_prods[i].id) {
+                var total = productPrices[j].price +
+                    productPrices[j].tax +
+                    productPrices[j].deliveryCharge -
+                    productPrices[j].discount;
+                prices.push(total);
+                stores_final.push(productPrices[j].storeId);
+            }
+        }
+
+        colElem.innerHTML = colElem.innerHTML + "<br />";
+        for (let j = 0; j < stores_final.length; j++) {
+            for (let k = 0; k < stores.length; k++) {
+                if (stores[k].id == stores_final[j]) {
+                    colElem.innerHTML = colElem.innerHTML + "<img src='./assets/" + stores[k].icon + "' class='icon-image'>";
+                    break;
+                }
+            }
+        }
+        rowElem.appendChild(colElem);
+
+
+        prices.sort();
+        colElem = document.createElement('td');
+        colElem.innerHTML = "Starting from <br /><strong>" + prices[0].toString() + "</strong>";
+        rowElem.appendChild(colElem);
+
+        tableElem.appendChild(rowElem);
     }
-  }
-  rowElem.appendChild(colElem);
-
-
-  prices.sort();
-  colElem = document.createElement('td');
-  colElem.innerHTML="Starting from <br /><strong>"+prices[0].toString()+"</strong>";
-  rowElem.appendChild(colElem);
-
-  tableElem.appendChild(rowElem);
-}
 
 }
 
